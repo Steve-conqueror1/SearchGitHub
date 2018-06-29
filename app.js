3f14046e053516ce4c45a3a70b0bce10ff70e782
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2018-06-29 15:29:30 +0300

    Implemented the user interface

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-//Inialize Github
-const github = new GitHub;
+//init GitHub
+const github = new Github;
+
+//init UI
+const ui = new UI;
+
 //Search input
-const searchUser = document.getElementById('search');
-
+const searchUser = document.getElementById('searchUser');
 //search input event listenner
-searchUser.addEventListener('keyup', (e)=>{
-//get input text
-const userText = e.target.value;
-if(userText !== ''){
-  // console.log(userText);
-  //Make http call
+searchUser.addEventListener('keyup', (e) => {
+  //get inpu text
+  const userText = e.target.value;
 
-  github.getUser(userText)
-  .then(data => {
-    if(data.profile.message === 'Not Found'){
-      //Show alert     
+  if(userText !== '')
+  {
+    // console.log(userText);
+    //Make  http call
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found'){
+        //Show Alert
 
-    }else{
-      //Show profile
+      }else{
+        //show the Profile
+        ui.showProfile(data.profile);
 
-    }
-  }) 
-}
-else{
- //clear profile 
-}
+      }
+    })    
+  } else{
+    //clear profile
+
+  }
 });
